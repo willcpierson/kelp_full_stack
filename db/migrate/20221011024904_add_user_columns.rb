d2dcd947fc39d29f7ce8d0a860a9350f19319139
Will Pierson <willcpierson@gmail.com>
2022-10-24 16:20:03 -0400

    adjust schema for users creation

--- db/migrate/20221011024904_add_user_columns.rb
+++ db/migrate/20221011024904_add_user_columns.rb
@@ -1,7 +1,15 @@
 class AddUserColumns < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :last_name, :string, null: false
-    add_column :users, :zip, :integer, null: false
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.integer :zip, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -69,13 +69,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_200417) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.integer "zip", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.integer "zip", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
