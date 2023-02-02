2bc88210d5779e755780781ae0794219c21e4432
Will Pierson <willcpierson@gmail.com>
2023-02-01 21:36:25 -0500

    start carousel creation on HomePage

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,12 +1,27 @@
 import styles from './HomePage.module.css';
 import SearchBar from "../SearchBar";
-
+import { useState, useEffect } from 'react';
 
 const HomePage = () => {
 
+    const [background, setBackground] = useState(0);
+    
+    // useEffect(() => {
+
+    // }, []);
+
+    const carouselHomePage = () => {
+        setInterval(
+        if (background < 2) {
+            setBackground(background + 1)
+        } else {
+            setBackground(0)
+        });
+    };
+
     return (
         <>
-            <div id={styles.homePage}> </div>
+            <div id={styles.homePage1}> </div>
             <SearchBar id={styles.searchBar}/>
         </>
     );
