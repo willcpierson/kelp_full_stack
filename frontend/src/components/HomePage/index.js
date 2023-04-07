01ad5efc4d0ee56545343ea0e37a95e7f601931e
Will Pierson <willcpierson@gmail.com>
2023-04-07 18:38:30 -0400

    fix businessPhoto sizing on BusinessListingItem, initiate preview of businesses on home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -5,44 +5,13 @@ import Carousel from '../Carousel';
 
 const HomePage = () => {
 
-    const [background, setBackground] = useState(0);
-
-
-    // NEAR COMPLETION OF CAROUSEL BELOW! Seems to get faster longer you sit on the page; negative feedback cycle?
-    // useEffect(() => {
-    //     setInterval(() => {
-    //         if (background < 2) {
-    //             setBackground((background + 1));
-    //         } else {
-    //             setBackground(0);
-    //         };
-    //     }, 5000)
-    // }, [background]);
-
-    const carouselHomePage = () => {
-        console.log(background)
-            if (background === 0) {
-                return (
-                    <div id={styles.homePage1}> </div>
-                );
-            } else if (background === 1) {
-                return (
-                    <div id={styles.homePage2}> </div>
-                );
-            } else if (background === 2) {
-                return (
-                    <div id={styles.homePage3}> </div>
-                );
-            };
-    };
-
     return (
         <>
-            {/* <div id={styles.homePage1}> </div> */}
             <div id={styles.container}>
                 <SearchBar id={styles.searchBar}/>
                 <Carousel />
             </div>
+            <h2 id={styles.nextReviewHeader}>Your Next Review Awaits</h2>
         </>
     );
 };
