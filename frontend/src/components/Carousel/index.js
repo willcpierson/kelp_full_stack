ecacafebd07470757bea143d4347b2cda06f078c
Will Pierson <willcpierson@gmail.com>
2023-04-17 17:56:41 -0400

    temporarily disable carousel

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -11,30 +11,30 @@ const Carousel = () => {
         'https://oceanfdn.org/wp-content/uploads/2010/08/SargassoSea-1-1200x600.jpg'
     ];
 
-    const [imageIndex, setImageIndex] = useState(0);
+    // const [imageIndex, setImageIndex] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-          setImageIndex((imageIndex + 1) % backgroundImages.length);
-          console.log(`loading ${imageIndex}...`)
-        }, 9000);
-        return () => clearInterval(interval);
-      }, [imageIndex]);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //       setImageIndex((imageIndex + 1) % backgroundImages.length);
+    //       console.log(`loading ${imageIndex}...`)
+    //     }, 9000);
+    //     return () => clearInterval(interval);
+    //   }, [imageIndex]);
 
-    const setFollowingDivImage = () => {
-        console.log('loading following image...')
-        if (imageIndex === 2) {
-            return 0;
-        } else {
-            return imageIndex + 1;
-        };
-    };
+    // const setFollowingDivImage = () => {
+    //     console.log('loading following image...')
+    //     if (imageIndex === 2) {
+    //         return 0;
+    //     } else {
+    //         return imageIndex + 1;
+    //     };
+    // };
 
     return (
         <div className={styles.carouselContainer}>
             <div className={styles.currentBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[0]})` }} />
-            <div className={styles.followingBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[1]})` }}/>
-            <div className={styles.followingBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[2]})` }}/>
+            {/* <div className={styles.followingBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[1]})` }}/>
+            <div className={styles.followingBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[2]})` }}/> */}
         </div>
     );
 };
