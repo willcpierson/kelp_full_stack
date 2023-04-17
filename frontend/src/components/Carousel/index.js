bac894cd4756371b76dcbcee812500dce3c120cd
Will Pierson <willcpierson@gmail.com>
2023-04-17 18:37:27 -0400

    change svg colors, background brightness, add hero text

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 const Carousel = () => {
 
     const backgroundImages = [
-        'https://ocean.si.edu/sites/default/files/styles/article_main_image/public/2018-12/turtle-863336_1280.jpg.webp?itok=LBC4-xez',
+        'https://www.visitphilly.com/wp-content/uploads/2017/12/AdventureAquarium_tunnel-J_Fusco-for-vp-2200x1237px.jpg',
         'https://images.unsplash.com/photo-1488188840666-e2308741a62f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b2NlYW58ZW58MHx8MHx8&w=1000&q=80',
         'https://oceanfdn.org/wp-content/uploads/2010/08/SargassoSea-1-1200x600.jpg'
     ];
@@ -33,6 +33,11 @@ const Carousel = () => {
     return (
         <div className={styles.carouselContainer}>
             <div className={styles.currentBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[0]})` }} />
+            <div className={styles.textContainer}>
+                    <h2 className={styles.heroText}>
+                        Explore fantastical aquariums
+                    </h2>
+                </div>
             {/* <div className={styles.followingBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[1]})` }}/>
             <div className={styles.followingBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[2]})` }}/> */}
         </div>
--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -106,20 +106,21 @@ const SearchBar = () => {
                 </aside>
             </form>
             <form id={styles.subsearches} onSubmit={handleSpecificSubmit}>
-                <Link to={ { pathname: '/listings/food' } } food={food} location={cityState} className={styles.subSearch}>Seafood</Link>
+                <Link to={ { pathname: '/listings/food' } } food={food} location={cityState} className={styles.subSearch}>Seafood
                 <svg className={styles.downArrow}>
-                  <path d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
+                <path className={styles.pathDownArrow} d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
                   </svg>
+                  </Link>
                 <Link to={ { pathname: '/listings/aquarium' } } food={food} location={cityState} className={styles.subSearch}>
                   Aquariums
                   <svg className={styles.downArrow}>
-                  <path d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
+                  <path className={styles.pathDownArrow} d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
                   </svg>
                 </Link>
                 <Link to={ { pathname: '/listings/pier' } } food={food} location={cityState} className={styles.subSearch}>
                   Piers
                   <svg className={styles.downArrow}>
-                  <path d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
+                  <path className={styles.pathDownArrow} d="M12 15.25a1 1 0 01-.7-.29l-4.58-4.5A1.011 1.011 0 018.12 9L12 12.85 15.88 9a1 1 0 111.4 1.42L12.7 15a1 1 0 01-.7.25z"></path>
                   </svg>
                 </Link>
 
