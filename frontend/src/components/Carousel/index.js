833459d149f93171d25cf47c3c3855127cba6f09
Will Pierson <willcpierson@gmail.com>
2023-05-01 16:04:03 -0400

    add stylized search example button on homepage

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './Carousel.module.css';
 
 const Carousel = () => {
@@ -11,10 +12,16 @@ const Carousel = () => {
     return (
         <div className={styles.carouselContainer}>
             <div className={styles.currentBackgroundImage} style={{ backgroundImage: `url(${backgroundImages[0]})` }} />
-            <div className={styles.textContainer}>
+                <div className={styles.textContainer}>
                     <h2 className={styles.heroText}>
                         Explore fantastical aquariums
                     </h2>
+                    <Link id={styles.findAquariumsButton} to={ { pathname: '/listings/aquarium' } }>
+                        <svg id={styles.searchIcon}>
+                            <path d="M21.853 20.355l-3.444-3.443a9.428 9.428 0 10-16.761-6.171 9.428 9.428 0 0015.348 7.586l3.443 3.442a1 1 0 101.414-1.414zM5.82 16.245a7.429 7.429 0 115.253 2.175 7.38 7.38 0 01-5.253-2.176z"></path>
+                        </svg>
+                        <p id={styles.buttonText}>Aquariums</p>
+                    </Link>
                 </div>
         </div>
     );
