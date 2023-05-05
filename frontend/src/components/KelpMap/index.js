ac63509fc5fbb425ee37afc03e9687ee55528384
Will Pierson <willcpierson@gmail.com>
2023-05-04 22:25:53 -0400

    import wrapper and add to kelpmap

--- frontend/src/components/KelpMap/index.js
+++ frontend/src/components/KelpMap/index.js
@@ -1,1 +1,12 @@
 import styles from './KelpMap.module.css';
+import { Wrapper, GoogleMap } from '@googlemaps/react-wrapper';
+
+const KelpMap = () => {
+
+    return (
+        <>
+        </>
+    );
+};
+
+export default KelpMap;
