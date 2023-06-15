67436bc9263db06cdb10229cf2267be38f5b94fd
Will Pierson <willcpierson@gmail.com>
2023-06-15 20:42:43 +0000

    import Map Marker(s)

--- frontend/src/components/KelpMap/index.js
+++ frontend/src/components/KelpMap/index.js
@@ -1,10 +1,11 @@
 import styles from './KelpMap.module.css';
 import { Wrapper } from '@googlemaps/react-wrapper';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const KelpMap = () => {
 
     const API_KEY = process.env.REACT_APP_GOOGLEMAPS_API_KEY;
+    
 
     const containerStyle = {
         width: '455px',
