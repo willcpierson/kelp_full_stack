bdedee6a6b01c5807fb99815be28cd739badbd0b
Will Pierson <willcpierson@gmail.com>
2023-07-05 02:45:13 +0000

    connect appropriate businesses to HomePageBusinessPreviewItems

--- frontend/src/components/HomePageBusinessItem/index.js
+++ frontend/src/components/HomePageBusinessItem/index.js
@@ -1,13 +1,26 @@
 import styles from './HomePageBusinessItem.module.css'
+import { getBusiness,fetchBusiness } from '../../store/businesses';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 
-const HomePageBusinessItem = () => {
+const HomePageBusinessItem = (props) => {
+    
+    const dispatch = useDispatch();
+    const business = useSelector(getBusiness(props.business));
+
+    useEffect(() => {
+        if (!business) {
+            dispatch(fetchBusiness(props.business))
+        }
+    }, []);
 
 
     return (
         <div className={styles.homePageBusinessItemContainer}>
-                <h2>Business Item</h2>
+                <h2>Business Item {props.business}</h2>
+                <p>{business.name}</p>
         </div>
     );
 };
--- frontend/src/components/HomePageBusinessPreview/index.js
+++ frontend/src/components/HomePageBusinessPreview/index.js
@@ -4,29 +4,19 @@ import { getBusinesses } from '../../store/businesses';
 import { useSelector } from 'react-redux';
 
 const HomePageBusinessPreview = () => {
-
-    // const businesses = useSelector(getBusinesses())
-
-    // const mappedBusinesses = businesses.map((business) => {
-    //     return <HomePageBusinessItem business={business} key={business.id} />
-    // });
-
-
     return (
         <div id={styles.HomePageBusinessPreviewContainer}>
             <h1 id={styles.checkOutTheseBusinesses}>Check out these businesses</h1>
             <div id={styles.grid}>
                 {/* {Note that CSS changes to this div are not reflective yet, ask help from tech coach} */}
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
-                <HomePageBusinessItem />
+                <HomePageBusinessItem business={1} />
+                <HomePageBusinessItem business={2} />
+                <HomePageBusinessItem business={3} />
+                <HomePageBusinessItem business={4} />
+                <HomePageBusinessItem business={5} />
+                <HomePageBusinessItem business={6} />
+                <HomePageBusinessItem business={7} />
+                <HomePageBusinessItem business={8} />
             </div>
             {/* {mappedBusinesses} */}
         </div>
