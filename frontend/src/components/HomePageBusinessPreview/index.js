98196ec8cb4f0b5d8a8e0afb21ca0bb94020b5ac
Will Pierson <willcpierson@gmail.com>
2023-07-19 20:22:01 +0000

    almost fix final businesshomepagepreview issue CSS wise

--- frontend/src/components/HomePageBusinessPreview/index.js
+++ frontend/src/components/HomePageBusinessPreview/index.js
@@ -1,13 +1,11 @@
-import styles from './HomePageBusinessPreview.module.css'
+import styles from './HomePageBusinessPreview.module.css';
 import HomePageBusinessItem from "../HomePageBusinessItem";
-import { getBusinesses } from '../../store/businesses';
-import { useSelector } from 'react-redux';
 
 const HomePageBusinessPreview = () => {
     return (
         <div id={styles.HomePageBusinessPreviewContainer}>
             <h1 id={styles.checkOutTheseBusinesses}>Check out these businesses</h1>
-            <div id={styles.grid}>
+            <div className={styles.homePageBusinesses}>
                 {/* {Note that CSS changes to this div are not reflective yet, ask help from tech coach} */}
                 <HomePageBusinessItem business={1} />
                 <HomePageBusinessItem business={2} />
@@ -18,7 +16,6 @@ const HomePageBusinessPreview = () => {
                 <HomePageBusinessItem business={7} />
                 <HomePageBusinessItem business={8} />
             </div>
-            {/* {mappedBusinesses} */}
         </div>
     );
 };
