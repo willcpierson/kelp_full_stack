bb3938789c79909560a70db6fd372b55690f0aaf
Will Pierson <willcpierson@gmail.com>
2023-07-22 00:41:06 +0000

    finish all but main page

--- frontend/src/components/HomePageBusinessPreview/index.js
+++ frontend/src/components/HomePageBusinessPreview/index.js
@@ -3,10 +3,12 @@ import HomePageBusinessItem from "../HomePageBusinessItem";
 
 const HomePageBusinessPreview = () => {
     return (
+        <>
         <div id={styles.HomePageBusinessPreviewContainer}>
             <h1 id={styles.checkOutTheseBusinesses}>Check out these businesses</h1>
-            <div className={styles.homePageBusinesses}>
-                {/* {Note that CSS changes to this div are not reflective yet, ask help from tech coach} */}
+           
+        </div>
+        <div className={styles.homePageBusinesses}>
                 <HomePageBusinessItem business={1} />
                 <HomePageBusinessItem business={2} />
                 <HomePageBusinessItem business={3} />
@@ -16,7 +18,7 @@ const HomePageBusinessPreview = () => {
                 <HomePageBusinessItem business={7} />
                 <HomePageBusinessItem business={8} />
             </div>
-        </div>
+        </>
     );
 };
 
