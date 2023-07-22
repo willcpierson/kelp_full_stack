e0623ded4c5a998be13c7e2c83ceb5575b439339
Will Pierson <willcpierson@gmail.com>
2023-07-22 01:44:41 +0000

    finalize homepagebusinesspreviewitems

--- frontend/src/components/HomePageBusinessPreview/index.js
+++ frontend/src/components/HomePageBusinessPreview/index.js
@@ -2,13 +2,14 @@ import styles from './HomePageBusinessPreview.module.css';
 import HomePageBusinessItem from "../HomePageBusinessItem";
 
 const HomePageBusinessPreview = () => {
+    console.log(styles);
     return (
         <>
         <div id={styles.HomePageBusinessPreviewContainer}>
             <h1 id={styles.checkOutTheseBusinesses}>Check out these businesses</h1>
            
         </div>
-        <div className={styles.homePageBusinesses}>
+        <div id={styles.homePageBusinesses}>
                 <HomePageBusinessItem business={1} />
                 <HomePageBusinessItem business={2} />
                 <HomePageBusinessItem business={3} />
@@ -17,7 +18,8 @@ const HomePageBusinessPreview = () => {
                 <HomePageBusinessItem business={6} />
                 <HomePageBusinessItem business={7} />
                 <HomePageBusinessItem business={8} />
-            </div>
+        </div>
+    
         </>
     );
 };
